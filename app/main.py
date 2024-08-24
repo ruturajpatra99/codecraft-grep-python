78ab36c7b2a7aa392df3a0ff9b57ca0fb5b783fd
P Ruturaj Patra <ruturajpatra99@gmail.com>
2024-08-24 21:37:20 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ import sys
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif pattern == "\d":
+        return input_line.isnumeric()
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
