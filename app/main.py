2871e766371e3ab2e630cefd0dc4c6fcba04090b
P Ruturaj Patra <ruturajpatra99@gmail.com>
2024-08-24 23:27:02 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,49 +1,47 @@
 import sys
-
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
-
 class Pattern:
     DIGIT = "\d"
     ALNUM = "\w"
-
-
+# def combined_patterns(input_line, pattern):
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
-    elif pattern == "\d":
-        return any(char.isdigit() for char in input_line)
-    elif pattern == "\w":
-        return any(char.isalnum() for char in input_line)
-    elif pattern[0] == "[" and pattern[-1]=="]":
-        if pattern[1]=="^":
-            return not any(char in pattern[1:-1] for char in input_line)
-        else: return any(char in pattern[1:-1] for char in input_line)
-    
-    if len(input_line)==0 and len(pattern)==0:
+    elif pattern == Pattern.DIGIT:
+        return any(c.isdigit() for c in input_line)
+    elif pattern == Pattern.ALNUM:
+        return any(c.isalnum() for c in input_line)
+    if len(input_line) == 0 and len(pattern) == 0:
         return True
     if not pattern:
         return True
     if not input_line:
         return False
-    
-    if pattern[0]==input_line[0]:
+    if pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == "\d":
+    elif pattern[:2] == Pattern.DIGIT:
         for i in range(len(input_line)):
             if input_line[i].isdigit():
                 return match_pattern(input_line[i:], pattern[2:])
-            else:
-                return False
-    elif pattern[:2] == "\w":
+        else:
+            return False
+    elif pattern[:2] == Pattern.ALNUM:
         if input_line[0].isalnum():
             return match_pattern(input_line[1:], pattern[2:])
         else:
             return False
+
+    elif pattern[0] == "[" and pattern[-1]=="]":
+        if pattern[1]=="^":
+            return not any(char in pattern[1:-1] for char in input_line)
+        else: return any(char in pattern[1:-1] for char in input_line)
+    
+    
     
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
-    return match_pattern(input_line[1:], pattern)
+        return match_pattern(input_line[1:], pattern)
 
 
 def main():
