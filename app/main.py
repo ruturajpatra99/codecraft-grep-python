2ecc268093d1622cd6dd375572671558810f85e8
P Ruturaj Patra <ruturajpatra99@gmail.com>
2024-08-24 21:55:58 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -13,6 +13,8 @@ def match_pattern(input_line, pattern):
         return input_line.isalnum()
     elif pattern[0] == "[" and pattern[-1]=="]":
         return any(char in pattern[1:-1] for char in input_line)
+    elif pattern[0] == "[" and pattern[-1]=="]":
+        return any(char not in pattern[1:-1] for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
