cd5f5df4d3fff158a346e403fa2a2ceab7aee1ba
P Ruturaj Patra <ruturajpatra99@gmail.com>
2024-08-25 16:16:34 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -77,20 +77,13 @@ def match_pattern(input_line: str, pattern_list: typing.List[str]) -> bool:
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    if match_pattern(input_line, pattern):
+    pattern_list = tokenize_regex(pattern)
+    if match_pattern(input_line, pattern_list):
         exit(0)
     else:
         exit(1)
-
-
 if __name__ == "__main__":
     main()
