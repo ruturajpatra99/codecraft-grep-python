894c023946009c2ef20217e4d00e8c77dce9b649
P Ruturaj Patra <ruturajpatra99@gmail.com>
2024-08-25 16:13:33 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,57 +1,78 @@
+# import collections
+import string
 import sys
+import typing
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
-class Pattern:
-    DIGIT = r"\d"
-    ALNUM = r"\w"
-# def combined_patterns(input_line, pattern):
-def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == Pattern.DIGIT:
-        return any(c.isdigit() for c in input_line)
-    elif pattern == Pattern.ALNUM:
-        return any(c.isalnum() for c in input_line)
-    if len(input_line) == 0 and len(pattern) == 0:
+NUMBERS = string.digits
+ALPHANUMERIC = string.ascii_letters + NUMBERS + "_"
+
+def tokenize_regex(pattern: str) -> typing.List[str]:
+    tokens = []
+    i = 0
+    if pattern[0] == "^":  # Start of Anchor
+        tokens.append(pattern)
+        i += len(pattern)
+    if pattern[-1] == "$":  # End of Anchor
+        tokens.append(pattern)
+        i += len(pattern)
+
+    while i < len(pattern):
+        if pattern[i] == "\\":
+            tokens.append(pattern[i : i + 2])
+            i += 2
+        elif pattern[i] == "[":
+            end_index = i + 1
+            while pattern[end_index] != "]":
+                end_index += 1
+            tokens.append(pattern[i : end_index + 1])
+            i = end_index + 1
+        else:
+            tokens.append(pattern[i])
+            i += 1
+    return tokens
+
+
+def match_pattern(input_line: str, pattern_list: typing.List[str]) -> bool:
+    if len(input_line) == 0 and len(pattern_list) == 0:
         return True
-    if not pattern:
+    if len(pattern_list) == 0:
         return True
     if not input_line:
         return False
-    if pattern[0] == input_line[0]:
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == Pattern.DIGIT:
+    if pattern_list[0][0] == "^":
+        return input_line.startswith(pattern_list[0][1:])
+    
+    if pattern_list[0][-1] == "$":
+        return input_line.endswith(pattern_list[0][:-1])
+    if pattern_list[0] == input_line[0]:
+        return match_pattern(input_line[1:], pattern_list[1:])
+    elif pattern_list[0] == "\\d":
         for i in range(len(input_line)):
             if input_line[i].isdigit():
-                return match_pattern(input_line[i:], pattern[2:])
+                return match_pattern(input_line[i + 1 :], pattern_list[1:])
         else:
             return False
-    elif pattern[:2] == Pattern.ALNUM:
-        if input_line[0].isalnum():
-            return match_pattern(input_line[1:], pattern[2:])
+    elif pattern_list[0] == "\\w":
+        for i in range(len(input_line)):
+            if input_line[i].isalnum():
+                return match_pattern(input_line[i + 1 :], pattern_list[1:])
         else:
             return False
-
-    elif pattern[0] == "[" and pattern[-1]=="]":
-        if pattern[1]=="^":
-            return not any(char in pattern[1:-1] for char in input_line)
-        else: return any(char in pattern[1:-1] for char in input_line)
-    
-    elif pattern[0]=="^":
-        if pattern[1]==input_line[0]:
-            return True
-        else: return False
-    
-    elif pattern[-1] == "$":
-        l = len(pattern[:-1])
-        if input_line[-l:] == pattern[:-1]:
-            return True
-        return False
-    
-    else:
-        return match_pattern(input_line[1:], pattern)
-    
-
+    elif pattern_list[0][0] == "[" and pattern_list[0][-1] == "]":
+        if pattern_list[0][1] == "^":
+            for i in range(len(input_line)):
+                if input_line[i] not in pattern_list[0][2:-1]:
+                    return match_pattern(input_line[i + 1 :], pattern_list[1:])
+            else:
+                return False
+        else:
+            for i in range(len(input_line)):
+                if input_line[i] in pattern_list[0][1:-1]:
+                    return match_pattern(input_line[i + 1 :], pattern_list[1:])
+            else:
+                return False
+    return match_pattern(input_line[1:], pattern_list)
 
 def main():
     pattern = sys.argv[2]
